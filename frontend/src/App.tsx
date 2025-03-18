56c68d268f3d1484d9add4cb2b2c35c00a823e2e
Vakci <55985512+Vakcika@users.noreply.github.com>
2025-03-18 10:15:19 +0100

    login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Page404 from "./pages/Page404";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,98 @@
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Form, Formik } from "formik";
+import * as Yup from "yup";
+import LoginBrand from "@/components/common/LoginBrand";
+import LazyImage from "@/components/common/LazyImg";
+
+export default function Login() {
+  const handleLogin = async (credentials: object) => {};
+
+  return (
+    <>
+      <title>CRM Portal | Login</title>
+      <link rel="canonical" href="/login"></link>
+      <meta name="description" content="CRM Portal login page" />
+      <div className="flex flex-col md:flex-row items-center justify-auto">
+        <LazyImage
+          src="/images/background_login.webp"
+          alt="Login Background"
+          className="w-full h-screen hidden md:block"
+          width="w-full"
+          height="h-screen"
+        />
+        <div className="flex flex-col w-full items-center justify-auto h-screen">
+          <div className="w-full h-full max-w-sm p-4 flex flex-col justify-center">
+            <div>
+              <LoginBrand />
+              <Formik
+                initialValues={{ email: "", password: "" }}
+                validationSchema={Yup.object({
+                  email: Yup.string()
+                    .email("Invalid email address")
+                    .required("Required"),
+                  password: Yup.string().required("Required"),
+                })}
+                onSubmit={handleLogin}
+              >
+                {({
+                  values,
+                  errors,
+                  touched,
+                  handleChange,
+                  handleBlur,
+                  isSubmitting,
+                }) => (
+                  <Form className="my-16">
+                    <div className="space-y-6">
+                      <div className="space-y-2">
+                        <Label htmlFor="email">Email address</Label>
+                        <Input
+                          id="email"
+                          name="email"
+                          type="email"
+                          placeholder="Enter email"
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.email}
+                        />
+                        {touched.email && errors.email && (
+                          <p className="text-sm text-w300">{errors.email}</p>
+                        )}
+                      </div>
+
+                      <div className="space-y-2">
+                        <Label htmlFor="password">Password</Label>
+                        <Input
+                          id="password"
+                          name="password"
+                          type="password"
+                          placeholder="Enter password"
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.password}
+                        />
+                        {touched.password && errors.password && (
+                          <p className="text-sm text-w300">{errors.password}</p>
+                        )}
+                      </div>
+                    </div>
+
+                    <Button
+                      type="submit"
+                      className="w-full mt-8"
+                      disabled={isSubmitting}
+                    >
+                      {isSubmitting ? "Logging in..." : "Login"}
+                    </Button>
+                  </Form>
+                )}
+              </Formik>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
