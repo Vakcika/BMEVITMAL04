a3e112863bb4d60c476e8e86dfea95afd42169d4
Vakci <55985512+Vakcika@users.noreply.github.com>
2025-03-18 10:27:31 +0100

    applayout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import Page404 from "./pages/Page404";
 import Login from "./pages/Login";
+import { Suspense } from "react";
+import LoadingCircle from "./components/common/LoadingCircle";
+import SideBar from "./components/sidebar/Sidebar";
+
+function AppLayout() {
+  return (
+    <div className="sm:ml-16 sm:pl-20 lg:ml-48 m-4">
+      <Suspense fallback={<LoadingCircle />}>
+        <SideBar />
+        <Outlet />
+      </Suspense>
+    </div>
+  );
+}
 
 function App() {
   return (
@@ -9,6 +23,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/app" element={<AppLayout />}>
+          <Route path="dashboard" element={<Page404 />} />
+        </Route>
         <Route path="*" element={<Page404 />} />
       </Routes>
     </BrowserRouter>
