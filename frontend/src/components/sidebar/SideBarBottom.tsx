e96fe65b9f2ac33a54d4cfd5c4f4445290781186
Vakci <55985512+Vakcika@users.noreply.github.com>
2025-04-17 12:53:14 +0200

    logout button separation

--- frontend/src/components/sidebar/SideBarBottom.tsx
+++ frontend/src/components/sidebar/SideBarBottom.tsx
@@ -1,37 +1,9 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "@webbydevs/react-laravel-sanctum-auth";
-import { LogOut } from "lucide-react";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-} from "@/components/ui/alert-dialog";
-import useHttpPost from "@/api/useHttpPost";
-import { toast } from "sonner";
 import LazyImage from "../common/LazyImg";
+import { useAuth } from "@webbydevs/react-laravel-sanctum-auth";
+import LogoutButton from "../common/LogoutButton";
 
 export default function SideBarBottom() {
   const { user } = useAuth();
-  const navigate = useNavigate();
-  const [isLogoutDialogOpen, setIsLogoutDialogOpen] = useState(false);
-  const logoutMutation = useHttpPost("/api/logout");
-
-  const handleLogout = async () => {
-    try {
-      await logoutMutation.mutateAsync(user);
-      setIsLogoutDialogOpen(false);
-      toast("You have been logged out!");
-      navigate("/login");
-    } catch (error) {
-      console.error("Logout failed:", error);
-    }
-  };
 
   return (
     <div className="fixed bottom-0 left-0 right-0 z-20 bg-n10">
@@ -53,41 +25,9 @@ export default function SideBarBottom() {
           <span className="text-sm block sm:hidden lg:block">
             {user?.user?.name}
           </span>
-          <button
-            type="button"
-            className="block sm:hidden lg:flex items-center justify-center"
-            aria-label="Logout"
-            onClick={() => setIsLogoutDialogOpen(true)}
-          >
-            <LogOut className="w-6 h-6" />
-          </button>
+          <LogoutButton />
         </div>
       </div>
-
-      <AlertDialog
-        open={isLogoutDialogOpen}
-        onOpenChange={setIsLogoutDialogOpen}
-      >
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>
-              Are you sure you want to log out?
-            </AlertDialogTitle>
-            <AlertDialogDescription>
-              You will need to log in again to access your account.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction
-              onClick={handleLogout}
-              className="bg-p200 hover:bg-p300"
-            >
-              Log out
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
     </div>
   );
 }
