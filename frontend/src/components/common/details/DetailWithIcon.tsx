fcb80fc5127555d28c8b9b6b787124c5734005de
Vakci <55985512+Vakcika@users.noreply.github.com>
2025-04-23 01:15:30 +0200

    detail with icon fix

--- frontend/src/components/common/details/DetailWithIcon.tsx
+++ frontend/src/components/common/details/DetailWithIcon.tsx
@@ -11,7 +11,7 @@ export default function DetailWithIcon({
     <div className="flex items-start">
       <div className="mt-1 mr-3">{icon}</div>
       <div>
-        <p className="text-sm font-medium text-n100">{label}</p>
+        <p className="text-sm font-medium text-n100 mb-1">{label}</p>
         <span className="text-lg">{value}</span>
       </div>
     </div>
