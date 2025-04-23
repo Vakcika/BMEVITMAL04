d686be3446cf92f6cd6b6538a9fe2d889df63a72
Vakci <55985512+Vakcika@users.noreply.github.com>
2025-04-23 10:47:25 +0200

    viewcustomer refactor

--- frontend/src/app/customers/ViewCustomer.tsx
+++ frontend/src/app/customers/ViewCustomer.tsx
@@ -1,31 +1,13 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { toast } from "sonner";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import {
-  Edit,
-  Globe,
-  Building,
-  Phone,
-  Mail,
-  User,
-  CalendarClock,
-  Hash,
-  UserCheck,
-} from "lucide-react";
 import useHttpGet from "@/api/useHttpGet";
-import CustomBreadCrumb from "@/components/common/CustomBreadcrumb";
-import LoadingCircle from "@/components/common/LoadingCircle";
-import { DeleteActionButton } from "@/components/tables/actions/DeleteActionButton";
 import useHttpDelete from "@/api/useHttpDelete";
-import EmailLink from "@/components/common/links/EmailLink";
-import PhoneLink from "@/components/common/links/PhoneLink";
-import WebsiteLink from "@/components/common/links/WebsiteLink";
-import Detail from "@/components/common/details/Detail";
-import DetailWithIcon from "@/components/common/details/DetailWithIcon";
-import CustomerStatusBadge from "@/components/common/badges/CustomerStatusBadge";
-import UserAvatar from "@/components/common/UserAvatar";
+import LoadingCircle from "@/components/common/LoadingCircle";
+import CustomBreadCrumb from "@/components/common/CustomBreadcrumb";
+import ViewCustomerHeader from "./ViewCustomerHeader";
+import CustomerDetailsCard from "./CustomerDatilsCard";
+import CustomerExtraInfoCard from "./CustomerExtraInfoCard";
 
 export default function ViewCustomer() {
   const { id } = useParams();
@@ -33,7 +15,6 @@ export default function ViewCustomer() {
   const [customer, setCustomer] = useState<Customer | null>(null);
 
   const query = useHttpGet<{ data: Customer }>(`/api/customers/${id}`);
-
   const deleteMutation = useHttpDelete("/api/customers", query);
 
   useEffect(() => {
@@ -47,9 +28,7 @@ export default function ViewCustomer() {
     console.error(query.error);
   }
 
-  const handleEdit = () => {
-    navigate(`/app/customers/${id}/edit`);
-  };
+  const handleEdit = () => navigate(`/app/customers/${id}/edit`);
 
   const handleDelete = async (customer: Customer) => {
     try {
@@ -68,7 +47,7 @@ export default function ViewCustomer() {
 
   const breadcrumbs = [
     { label: "Customers", url: "/app/customers" },
-    { label: customer?.company_name ?? query.data?.data.company_name, url: "" },
+    { label: customer?.company_name ?? "Customer", url: "" },
   ];
 
   if (query.isLoading) return <LoadingCircle />;
@@ -77,101 +56,14 @@ export default function ViewCustomer() {
   return (
     <div className="p-4">
       <CustomBreadCrumb model={breadcrumbs} />
-      <div className="mt-6 mb-6 flex flex-col md:flex-row md:justify-between md:items-center gap-4">
-        <div>
-          <h1 className="text-2xl font-semibold">{customer.company_name}</h1>
-          <p className="text-n100">
-            {customer.name} • <CustomerStatusBadge status={customer.status} />
-          </p>
-        </div>
-        <div className="flex flex-wrap gap-2">
-          <Button onClick={handleEdit}>
-            <Edit className="w-6 h-6 mr-2" />
-            Edit
-          </Button>
-          {handleDelete && (
-            <DeleteActionButton
-              item={customer}
-              itemName="customer"
-              itemLabel={customer.company_name}
-              onDelete={handleDelete}
-              variant="lg"
-            />
-          )}
-        </div>
-      </div>
-
-      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
-        <Card className="bg-n0 rounded-lg shadow lg:col-span-2">
-          <CardHeader>
-            <CardTitle>Customer Details</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              <DetailWithIcon
-                icon={<Building className="w-6 h-6 text-n100" />}
-                label="Company"
-                value={customer.company_name}
-              />
-              <DetailWithIcon
-                icon={<UserCheck className="w-6 h-6 text-n100" />}
-                label="Assigned to"
-                value={<UserAvatar user={customer.user?.name} />}
-              />
-              <DetailWithIcon
-                icon={<User className="w-6 h-6 text-n100" />}
-                label="Contact Name"
-                value={customer.name}
-              />
-              <DetailWithIcon
-                icon={<Mail className="w-6 h-6 text-n100" />}
-                label="Email"
-                value={<EmailLink email={customer.email} />}
-              />
-              <DetailWithIcon
-                icon={<Phone className="w-6 h-6 text-n100" />}
-                label="Phone"
-                value={<PhoneLink phone={customer.phone_number} />}
-              />
-              <DetailWithIcon
-                icon={<Globe className="w-6 h-6 text-n100" />}
-                label="Website"
-                value={
-                  customer.website ? (
-                    <WebsiteLink url={customer.website} />
-                  ) : (
-                    "N/A"
-                  )
-                }
-              />
-              <DetailWithIcon
-                icon={<Hash className="w-6 h-6 text-n100" />}
-                label="Tax Number"
-                value={customer.tax_number ?? "N/A"}
-              />
-              <DetailWithIcon
-                icon={<CalendarClock className="w-6 h-6 text-n100" />}
-                label="Created At"
-                value={new Date(customer.created_at).toLocaleString()}
-              />
-            </div>
-          </CardContent>
-        </Card>
-
-        <Card className="bg-n0 rounded-lg shadow lg:col-span-2 xl:col-span-1">
-          <CardHeader>
-            <CardTitle>Additional Information</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="space-y-4">
-              <Detail label="Address" value={customer.address ?? "N/A"} />
-              <Detail
-                label="Description"
-                value={customer.description ?? "No description provided."}
-              />
-            </div>
-          </CardContent>
-        </Card>
+      <ViewCustomerHeader
+        customer={customer}
+        onEdit={handleEdit}
+        onDelete={handleDelete}
+      />
+      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 mt-6">
+        <CustomerDetailsCard customer={customer} />
+        <CustomerExtraInfoCard customer={customer} />
       </div>
     </div>
   );
