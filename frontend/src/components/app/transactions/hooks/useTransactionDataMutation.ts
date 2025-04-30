585ed002da034363fc71cd63edaafe90b016e3e7
Vakci <55985512+Vakcika@users.noreply.github.com>
2025-04-30 16:33:39 +0200

    show exchange rates

--- frontend/src/components/app/transactions/hooks/useTransactionDataMutation.ts
+++ frontend/src/components/app/transactions/hooks/useTransactionDataMutation.ts
@@ -3,7 +3,7 @@ import { useAuth } from "@webbydevs/react-laravel-sanctum-auth";
 import useHttpPut from "@/api/useHttpPut";
 import useHttpPost from "@/api/useHttpPost";
 import { Transaction } from "@/types/Transaction";
-import { toast } from "sonner";
+import useExchangeRate from "./useExchangeRate";
 
 interface TransactionApiData {
   id?: UUID;
@@ -21,44 +21,11 @@ interface TransactionApiData {
   created_at?: string;
   updated_at?: string;
 }
-
-/**
- * Get exchange rate for the specified currency to HUF
- * If a date is provided, it fetches historical data for that date
- * Otherwise, it uses the latest exchange rate
- */
-async function getExchangeRate(from: string, date: string): Promise<number> {
-  if (from === "HUF") return 1;
-
-  const response = await fetch(
-    `https://api.frankfurter.app/${date}?from=${from}`
-  );
-
-  if (!response.ok) {
-    throw new Error(
-      `Failed to fetch exchange rate for ${from}${date ? ` on ${date}` : ""}`
-    );
-  }
-
-  const data = await response.json();
-
-  if (!data.rates?.HUF) {
-    throw new Error(
-      `No HUF rate available for ${from}${date ? ` on ${date}` : ""}`
-    );
-  }
-  toast.info(
-    `The exhange rate for ${from} ${date ? ` on ${date}` : ""} was ${
-      data.rates.HUF
-    }`
-  );
-  return data.rates.HUF;
-}
-
 export function useTransactionMutations() {
   const { user } = useAuth();
   const updateMutation = useHttpPut("/api/transactions");
   const createMutation = useHttpPost("/api/transactions");
+  const { getExchangeRate } = useExchangeRate();
 
   const prepareTransactionData = async (
     values: Transaction
@@ -71,11 +38,16 @@ export function useTransactionMutations() {
     if (fromCurrencyCode === "HUF") {
       newValues.amount_in_base = values.amount;
     } else {
-      const rate = await getExchangeRate(
-        fromCurrencyCode,
-        values.transaction_date.split("T")[0]
-      );
-      newValues.amount_in_base = Math.round(values.amount * rate); // Round to 0 decimals
+      const transactionDate = values.transaction_date.split("T")[0];
+
+      const rate = await getExchangeRate(fromCurrencyCode, transactionDate);
+
+      // If rate is 0, it means there was an error
+      if (rate === 0) {
+        newValues.amount_in_base = values.amount;
+      } else {
+        newValues.amount_in_base = Math.round(values.amount * rate); // Round to 0 decimals
+      }
     }
 
     // Transform relationships to foreign keys
